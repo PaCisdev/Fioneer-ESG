cc452164153e2a9633f662ecf10abe9777103405
PaulinaCiesielska <111356315+PaulinaCiesielska@users.noreply.github.com>
2024-02-14 12:53:33 +0100

    Reademe and comment improved

--- tests/colorCheck.spec.ts
+++ tests/colorCheck.spec.ts
@@ -2,18 +2,19 @@ import { test, expect, Page } from '@playwright/test';
 
 test.describe('Verify button background color', async () =>{
   test('test2', async ({page}) =>{
+
   // Go to the SAP Fioneer page
   await page.goto('https://www.sapfioneer.com'); 
 
-  // Zlokalizuj element "Get in touch" za pomocą odpowiedniego selektora CSS
+  // Localize Button
   const button = await page.$('li.html.header-button-1 > div.header-button > a.button.primary');
 
   if (button !== null){
-  // Pobierz kolor tła przycisku bezpośrednio w kontekście przeglądarki
+  // Get button backgroundcolor 
   const buttonColor = await page.evaluate(button => {
-    // Pobierz styl tła przycisku
+    // Get style of button
     const computedStyle = window.getComputedStyle(button);
-    // Zwróć kolor tła przycisku
+    // Return button color
     return computedStyle.getPropertyValue('background-color');
   }, button);
     // Assert that the button color is yellow
--- tests/messageValidation.spec.ts
+++ tests/messageValidation.spec.ts
@@ -3,49 +3,40 @@ import { test, expect } from '@playwright/test';
 test.describe('erify validation message for incorrect email value', async () =>{
   test('Test3', async ({page}) =>{
 
-  // Go to the SAP Fioneer page
-  await page.goto('https://www.sapfioneer.com'); 
-  const button = await page.$('li.html.header-button-1 > div.header-button > a.button.primary');
-  // Click on the Get in touch button
-  //await page.locator('li.html.header-button-1 > div.header-button > a.button.primary[locator="masthead"]:has-text("Get in touch")').click();
-  if (button !== null){
- 
-  //await page.waitForSelector('a.button.primary[locator="masthead"]');
-  //const buttonLocator = page.locator('a.button.primary[locator="masthead"]');
-  await button.click();
-} else {
-  throw new Error("Element is null. Unable to call someFunction.");
-};
+    // Go to the SAP Fioneer page
+    await page.goto('https://www.sapfioneer.com'); 
+    const button = await page.$('li.html.header-button-1 > div.header-button > a.button.primary');
+    
+    // Click on the Get in touch button
+    if (button !== null){
+      await button.click();
+    } else {
+      throw new Error("Element is null. Unable to call someFunction.");
+      };
 
-  // Verify if the user has been redirected to the correct page
-  const redirectedUrl = page.url();
-  await expect(redirectedUrl).toContain('/contact');
+    // Verify if the user has been redirected to the correct page
+    const redirectedUrl = page.url();
+    await expect(redirectedUrl).toContain('/contact');
 
-  // Type incorrect email value in the Work email field
-// Switch to the iframe containing the email input field
-const frame2 = await page.frameLocator('iframe[title="Form 0"]').first();
-const emailInput = await frame2.getByLabel('Work email*');
+    // Type incorrect email value in the Work email field and submit
+    const frame2 = await page.frameLocator('iframe[title="Form 0"]').first();
+    const emailInput = await frame2.getByLabel('Work email*');
+    await emailInput.fill('342323');  
 
-// Fill the email input field with a value
-await emailInput.fill('342323');
+    const submitButton = await frame2.getByRole('button', { name: 'Submit' });
+    await submitButton.click();
 
-const submitButton = await frame2.getByRole('button', { name: 'Submit' });
+    // Wait for the validation message to appear
+    await frame2.frameLocator('ul.no-list.hs-error-msgs.inputs-list');
 
-// Click the submit button within the iframe
-await submitButton.click();
+    // Assert the presence of the error message
+    const errorMessages = await frame2.locator('ul.no-list.hs-error-msgs.inputs-list').count();
 
-  // Wait for the validation message to appear
- // Wait for the error message to appear
-await frame2.frameLocator('ul.no-list.hs-error-msgs.inputs-list');
-//if (button !== null){
-// Assert the presence of the error message
-const errorMessages = await frame2.locator('ul.no-list.hs-error-msgs.inputs-list').count();
+    // Check if error message is as on requirement
+    expect(errorMessages).toBeGreaterThan(0);
 
-// Sprawdź, czy żadne komunikaty błędów nie zostały znalezione
-expect(errorMessages).toBeGreaterThan(0);
-
-const errorMessageElement = await frame2.locator('ul.no-list.hs-error-msgs.inputs-list').first();
-const errorMessageText = await errorMessageElement.textContent();
+    const errorMessageElement = await frame2.locator('ul.no-list.hs-error-msgs.inputs-list').first();
+    const errorMessageText = await errorMessageElement.textContent();
 
 
 });
